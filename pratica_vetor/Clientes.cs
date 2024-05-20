facfde8e5f548f463b2efd6a1a4dc10564fb7db0
Matheus felipe <matheus.rodrigues304@etec.sp.gov.br>
2024-05-20 16:42:53 -0300

    Segundo Commit - Conclusao

---
+++ pratica_vetor/Clientes.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pratica_vetor
+{
+    class Cliente
+    {
+
+        public Cliente[] cli;
+        public string nome;
+        public int idade;
+        public string cpf;
+        public string rg;
+        public bool prioridade;
+        public int quantidadeFila = 0;
+        public int prioridades = 0;
+
+
+        
+
+        public Cliente(string nome = "", int idade = 0, string cpf = "", string rg = "", bool prioridade = false)
+        {
+            cli = new Cliente[10];
+            this.nome = nome;
+            this.idade =    idade;
+            this.cpf = cpf;
+            this.rg = rg;
+            this.prioridade = prioridade;
+        }
+
+
+        public void inserirCliente()
+
+        {
+            if(quantidadeFila < 10)
+            {
+            Console.WriteLine("Digite o seu nome:");
+            nome = Console.ReadLine();
+            Console.WriteLine("Digite a sua idade:");
+            idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o seu CPF");
+            cpf = Console.ReadLine();
+            Console.WriteLine("Digite o seu RG");
+            rg = Console.ReadLine();
+             Cliente cliente = new Cliente(nome, idade, cpf, rg);
+                cli[quantidadeFila] = cliente;
+                quantidadeFila++;
+            }
+            else
+            {
+                Console.WriteLine("Fila Cheia:");
+            }
+
+        }
+        public void listarCliente()
+        {
+            if(quantidadeFila  > 0) { 
+                for(int i = 0; i < cli.Length; i++) {
+                    if (cli[i] != null)
+                    {
+                    Console.WriteLine($"\nCliente {i+1}");
+                    Console.WriteLine($"Nome:{cli[i].nome}");
+                    Console.WriteLine($"Idade:{cli[i].idade}");
+                    Console.WriteLine($"CPF:{cli[i].cpf}");
+                    Console.WriteLine($"RG:{cli[i].rg}");
+                    Console.WriteLine($"Prioridade:{cli[i].prioridade}");
+                }
+                    }
+            }
+                else {
+                Console.WriteLine("Fila Vazia");
+            }
+        }
+        public void incluirPrioritario()
+        {
+            if(quantidadeFila < 10)
+            {
+                Console.WriteLine("Digite o seu nome:");
+                nome = Console.ReadLine();
+                Console.WriteLine("Digite a sua idade:");
+                idade = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o seu CPF");
+                cpf = Console.ReadLine();
+                Console.WriteLine("Digite o seu RG");
+                rg = Console.ReadLine();
+                Cliente clientePrioritario = new Cliente(nome, idade, cpf, rg, true);
+
+                for(int i  = quantidadeFila - 1; i >= prioridades ; i--)
+                {   
+                    cli[i+1] = cli[i];
+                }
+                cli[prioridades] = clientePrioritario;
+                prioridades++;
+                quantidadeFila++;
+            }
+        }
+
+        public void AtenderCliente()
+        {
+            Cliente clienteAtendido = cli[0];
+             
+            if( quantidadeFila > 0)
+            {
+                for(int i = 1; i < quantidadeFila; i++)
+                {
+                    cli[i-1] = cli[i];
+                }
+                    cli[quantidadeFila - 1] = null;
+                    quantidadeFila--;
+
+                Console.WriteLine($"O Cliente {clienteAtendido.nome} foi atendido com sucesso");
+            }
+            else
+            {
+                Console.WriteLine("Não ha Cliente");
+            }
+
+        }
+    }
+}
--- pratica_vetor/Program.cs
+++ pratica_vetor/Program.cs
@@ -10,6 +10,39 @@ namespace pratica_vetor
     {
         static void Main(string[] args)
         {
+
+            Cliente cliente = new Cliente();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            string opcao = "a";
+            while (opcao.ToLower() != "q")
+            {
+
+            Console.WriteLine("1. Cadastrar clientes\n" +
+                "2. Mostrar fila\n" +
+                "3. Adicionar Cliente com Prioridade\n" +
+                "4. Atender cliente\nDigite  a Letra 'q' para sair.");
+
+        opcao = Console.ReadLine();
+            switch (opcao)
+            {
+                case "1":
+                    cliente.inserirCliente();
+                    break;
+                case "2":
+                    cliente.listarCliente();
+                    break;
+                case "3":
+                    cliente.incluirPrioritario();
+                    break;
+                case "4":
+                        cliente.AtenderCliente();
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida. Tente novamente.");
+                    break;
+            }
+            }
         }
     }
 }
+    
